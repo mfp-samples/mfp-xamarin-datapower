1bf3befa3c672c866d00de484eb285c882f7f64f
Norton Samuel Augustus Stanley <norton@in.ibm.com>
2017-02-23 17:39:29 +0530

    Initial Commit

---
+++ Data/DataPowerChallengeHandler.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using Worklight;
+
+
+namespace WorklightSample
+{
+	public class DataPowerChallengeHandler : GatewayChallengeHandler
+	{
+		private static String securityCheckName = "LtpaBasedSSO";
+		private static DataPowerChallengeHandler ourInstance = new DataPowerChallengeHandler(securityCheckName);
+		private Dictionary<String, String> parms;
+		public static ManualResetEvent waitForPincode = new ManualResetEvent(false);
+
+		public static DataPowerChallengeHandler GetInstance()
+		{
+			return ourInstance;
+		}
+
+		private DataPowerChallengeHandler(String realm)
+		{
+			SecurityCheck = realm;
+		}
+
+		public override bool canHandleResponse(WorklightResponse response)
+		{
+			if (response == null
+			    || response.ResponseText == null ||
+			    response.ResponseText.IndexOf("j_security_check") == -1)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public override LoginFormInfo GetLoginFormParameters()
+		{
+			return LoginFormInfo;
+		}
+
+
+		public void SubmitLogin(String userName, String password)
+		{	
+			parms = new Dictionary<string, string>();
+			parms.Add("j_username", userName);
+			parms.Add("j_password", password);
+			LoginFormInfo = new LoginFormInfo("/j_security_check", parms, null, 0, "post");
+			SubmitLoginForm = true;
+			SubmitSuccess = true;
+			//ResourceRequestPage.ShowLoginLayout(true);
+			//ResourceRequestPage.ShowBalanceLayout(false);
+			waitForPincode.Set();
+
+		}
+
+		public string SecurityCheck
+		{
+			get; set;
+		}
+
+		public override string GetRealm()
+		{
+			return SecurityCheck;
+		}
+
+		private LoginFormInfo LoginFormInfo
+		{
+			get;
+			set;
+		}
+
+		public bool ShouldSubmitAnswer
+		{
+			get;
+			set;
+		}
+
+		private bool SubmitLoginForm
+		{
+			get;
+			set;
+		}
+
+		public override void HandleChallenge(WorklightResponse challenge)
+		{
+			waitForPincode.Reset();
+			ResourceRequestPage.ShowLoginLayout(true);
+			ResourceRequestPage.ShowBalanceLayout(false);
+			waitForPincode.WaitOne();
+		}
+
+		public override void OnFailure(WorklightResponse response)
+		{
+			SubmitSuccess = false;
+		}
+
+		public bool SubmitSuccess
+		{
+			get;
+			set;
+		}
+
+		public override void OnSuccess(WorklightResponse challenge)
+		{
+			SubmitSuccess = true;
+		}
+
+		public void SetSubmitCancelled()
+		{
+			SubmitSuccess = false; 
+			waitForPincode.Set();
+		}
+			
+
+		public override bool ShouldSubmitLoginForm()
+		{
+			return SubmitLoginForm;
+		}
+
+		public override bool ShouldSubmitSuccess()
+		{
+			return SubmitSuccess;
+		}
+
+		//public override bool ShouldSubmitChallengeAnswer()
+		//{
+		//	return ShouldSubmitAnswer;
+		//}
+
+		//public override JObject GetChallengeAnswer()
+		//{
+		//	return challengeAnswer;
+		//}
+
+
+	}
+}
---
+++ ResourceRequestPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WorklightSample
+{
+	public partial class ResourceRequestPage : ContentPage
+	{
+
+		private static StackLayout layout = null;
+		private static StackLayout balancelayoutpanel = null;
+		private DataPowerChallengeHandler datapowerChallengeHandler = null;
+		
+		
+		public ResourceRequestPage()
+		{
+			InitializeComponent();
+			getbalance.Clicked += OnGetBalanceButtonClicked;
+			login.Clicked += OnLoginClicked;
+			cancel.Clicked += OnCancelClicked;
+			layout = loginlayout;
+			balancelayoutpanel = balancelayout;
+			datapowerChallengeHandler = DataPowerChallengeHandler.GetInstance();
+			App.WorklightClient.client.RegisterChallengeHandler(datapowerChallengeHandler);
+		}
+
+	void OnCancelClicked(object sender, EventArgs e)
+	{
+			datapowerChallengeHandler.SetSubmitCancelled();
+			ShowLoginLayout(false);
+			ShowBalanceLayout(true);
+			progresstext.IsVisible = false;
+			getbalance.IsEnabled = true;
+
+	}
+
+		public static void ShowLoginLayout(bool visible)
+		{
+
+
+
+			Device.BeginInvokeOnMainThread(() =>
+		   {
+			   layout.IsVisible = visible;
+			
+
+   			});
+		}
+
+		public static void ShowBalanceLayout(bool visible)
+		{
+			
+			Device.BeginInvokeOnMainThread(() =>
+		   {
+				balancelayoutpanel.IsVisible = visible;
+
+
+		   });
+		}
+
+	void OnLoginClicked(object sender, EventArgs e)
+	{
+			
+			datapowerChallengeHandler.SubmitLogin(UserName.Text, Password.Text);
+			ShowLoginLayout(false);
+			ShowBalanceLayout(true);
+		
+		}
+
+		async void  OnGetBalanceButtonClicked(object sender, EventArgs e)
+		{
+			getbalance.IsEnabled = false;
+			balancetext.Text = "";
+			progresstext.IsVisible = true;
+			var result = await App.WorklightClient.ProtectedInvokeAsync();
+			progresstext.IsVisible = false;
+			balancetext.Text = result.Response;
+			getbalance.IsEnabled = true;	
+		}
+
+
+
+	}
+}
